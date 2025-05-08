4029975ba6366c445c2d4466835b56e1879b06d3
Robin Hagmayer <robin.hagmayer@proton.me>
2025-05-08 22:00:38 +0200

    don't install treesitter parser synchronously

--- nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -4,6 +4,7 @@ return {
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     opts = {
       ensure_installed = { 'bash', 'c', 'diff', "go", 'html', "java", 'lua', 'luadoc', 'markdown', 'markdown_inline', "python", 'query', 'vim', 'vimdoc' },
+      sync_install = false,
       highlight = { enable = true },
       indent = { enable = true },
     },
