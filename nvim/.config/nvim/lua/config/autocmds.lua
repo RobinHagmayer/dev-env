349a10dd5cb68f293ee5bff7b485d970a896b1b6
Robin Hagmayer <robin.hagmayer@proton.me>
2025-05-09 14:48:05 +0200

    feat(nvim): added autocmd foq closing help with 'q'

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,6 +1,15 @@
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 
+autocmd('FileType', {
+    group = augroup('close-with-q', { clear = true }),
+    desc = 'Close with <q>',
+    pattern = { 'help' },
+    callback = function(args)
+        vim.keymap.set('n', 'q', '<Cmd>quit<CR>', { buffer = args.buf })
+    end,
+})
+
 -- Highlight on yank
 autocmd("TextYankPost", {
   desc = "Highlight when yanking (copying) text",
