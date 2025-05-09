ac428a79316cf58451fd8af9f967ff1b7d767b57
Robin Hagmayer <robin.hagmayer@proton.me>
2025-05-09 14:33:35 +0200

    feat(nvim): removed checkhealth for providers

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -40,3 +40,9 @@ opt.backup = false
 opt.swapfile = false
 opt.writebackup = false
 opt.undofile = true
+
+-- Disable health checks for these providers.
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_node_provider = 0
