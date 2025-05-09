29b9d18aee8949f9518ffea8de9738e1aad689b4
Robin Hagmayer <robin.hagmayer@proton.me>
2025-05-09 22:46:44 +0200

    feat(nvim): added highlighting to references under the cursor

--- nvim/.config/nvim/lua/config/lsp.lua
+++ nvim/.config/nvim/lua/config/lsp.lua
@@ -21,6 +21,22 @@ local function on_attach(client, bufnr)
       require("fzf-lua").lsp_definitions({ jump1 = false })
     end, "Peek definition")
   end
+
+  if client:supports_method(methods.textDocument_documentHighlight) then
+    local under_cursor_highlights_group = vim.api.nvim_create_augroup("cursor-highlights", { clear = false })
+    vim.api.nvim_create_autocmd({ "CursorHold", "InsertLeave" }, {
+      group = under_cursor_highlights_group,
+      desc = "Highlight references under the cursor",
+      buffer = bufnr,
+      callback = vim.lsp.buf.document_highlight,
+    })
+    vim.api.nvim_create_autocmd({ "CursorMoved", "InsertEnter", "BufLeave" }, {
+      group = under_cursor_highlights_group,
+      desc = "Clear highlight references",
+      buffer = bufnr,
+      callback = vim.lsp.buf.clear_references,
+    })
+  end
 end
 
 -- Set max height and width for hover float.
