ab763592619d1886ed8520dfc83f3fb6efcfa613
Robin Hagmayer <robin.hagmayer@proton.me>
2025-05-19 17:52:04 +0200

    feat(nvim): added conform.nvim formatter plugin

---
+++ nvim/.config/nvim/lua/plugins/formatter.lua
@@ -0,0 +1,30 @@
+return {
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  keys = {
+    {
+      "<leader>cf",
+      function()
+        require("conform").format({ async = true })
+      end,
+      mode = "",
+      desc = "Format buffer",
+    },
+  },
+  ---@module "conform"
+  ---@diagnostic disable-next-line: undefined-doc-name
+  ---@type conform.setupOpts
+  opts = {
+    formatters_by_ft = {
+      go = { "gofmt" },
+      lua = { "stylua" },
+    },
+    default_format_opts = {
+      lsp_format = "fallback",
+    },
+    format_on_save = { timeout_ms = 500 },
+    -- Set log level
+    -- log_level = vim.log.levels.DEBUG,
+  },
+}
