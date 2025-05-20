841316f3c0af60a1846bb4fe0735eb379e2dc624
Robin Hagmayer <robin.hagmayer@proton.me>
2025-05-20 13:24:11 +0200

    feat(nvim): added kanagawa colorscheme for better highlighting

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,38 @@
 return {
   {
     "futsuuu/vim-robot",
+    -- lazy = false,
+    -- priority = 1000,
+    -- config = function()
+    --   vim.cmd([[colorscheme robot]])
+    -- end,
+  },
+  {
+    "rebelot/kanagawa.nvim",
     lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd([[colorscheme robot]])
+      require("kanagawa").setup({
+        -- Changes themes
+        colors = {
+          theme = {
+            all = {
+              ui = {
+                bg_gutter = "none",
+              },
+            },
+          },
+        },
+        -- Changes colors
+        overrides = function(colors)
+          local theme = colors.theme
+          return {
+            EndOfBuffer = { fg = theme.ui.nontext },
+          }
+        end,
+      })
+
+      vim.cmd([[colorscheme kanagawa-wave]])
     end,
   },
 }
